b326019ece3e2eb5a2f30db96a6f558a48fb9c80
OLopez_Admin <OLopez_Admin@nep-centralized.com>
2026-01-30 14:15:34 -0800

    Initial ASP.NET Core PasswordPortal

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PasswordPortalApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
